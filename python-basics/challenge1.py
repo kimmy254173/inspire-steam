b000b82f0589589b3b4a3bfafcfbd3433785c468
kimmy254173 <gitaukimberley@gmal.com>
2026-02-11 09:17:12 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+university = JKUAT
+Grade = "A"
+favourite-course = "machine learning engineering"
+cluster-points = 75
+
+print(f"my university is {university}")
+print(f"my grade is{grade}")
+print(f"my favourite course is{favourite course}")
+print(f"my cluster points are {points}")
