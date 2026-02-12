033235d7e4e48712f5473e42afbe595a2b292bf8
kimmy254173 <gitaukimberley@gmal.com>
2026-02-12 08:16:16 +0300

    input

---
+++ python-basics/numbers.py
@@ -0,0 +1,18 @@
+#Name : Kimberley Gitau
+# Date :11/02/2026
+
+# program to add , subtract ,divide ,multiply 2 numbers from input
+
+first_number = int(input("Enter the first number "))
+second_number = int(input("Enter the second number "))
+
+
+sum_number = first_number + second_number
+difference_number = first_number - second_number
+quotient_number = first_number / second_number
+product_number = first_number * second_number
+
+print(f"The sum of numbers : {sum_number}")
+print(f"The difference of numbers : {difference_number}")
+print(f"The product of numbers : {product_number}")
+print(f"The quotient of numbers :{quotient_number}")
---
+++ python-basics/user_input.py
@@ -0,0 +1,8 @@
+# Name: Kimberley Gitau
+# Date: 11/02/2026
+
+# program to get user input
+
+name = input("what is your name? ")
+
+print(name)
