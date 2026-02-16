2377aaf95d6faa2faddb330d22cd908a0d4b76d0
kimmy254173 <gitaukimberley@gmal.com>
2026-02-16 22:44:51 +0300

    tabulizing trigonometric functions

---
+++ python-basics/Assignment6.py
@@ -0,0 +1,12 @@
+# Name : Kimberley Gitau
+# Date : 16/02/2026
+# Program to tabulate trigonometric ratios
+
+import math
+print("_________________________________________________________________________")
+print(f"|  x   |   sin x        | cos x           |    tan x           |")
+for x in range(-180,210,30):
+    print(f"|   {x}    |   {math.sin(-180)} |   {math.cos(x)} |  {math.tan(x)} ")
+
+    for x in range(-180,180,30):
+        print(f"|   {x}   |   {math.sin(-180)}  |   {math.cos(x)} | {math.tan(x)} ")
