82f17a305e0b0a48994eb0867b2472c0d8c2a554
kimmy254173 <gitaukimberley@gmal.com>
2026-02-16 11:04:42 +0300

    income tax

--- python-basics/assignment5.py
+++ python-basics/assignment5.py
@@ -8,8 +8,17 @@ if salary <50000:
     tax = (2.5 * salary) / 100
     net_salary = salary - tax
 
+elif salary >50000 and salary <100000:
+    tax = (4.5 * salary) / 100
+    net_salary = salary - tax
+
+elif salary > 100000:
+    tax = (7.5 * salary) / 100
+    net_salary = salary - tax
+
 print(f"Gross salary = {salary}")
 print(f"Net salary = {net_salary}")
 print(f"Tax = {tax}")
 
+ 
 
